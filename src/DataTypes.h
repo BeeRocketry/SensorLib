792f83888fcadf0cd7dcc49038bb5bb33f53b05e
baftii <bartu.ozcan2004@gmail.com>
2025-07-31 18:18:23 +0300

    fix: DataTypes structlarındaki padding packing yöntemiyle engellendi.

--- src/DataTypes.h
+++ src/DataTypes.h
@@ -1,31 +1,39 @@
 #ifndef DATATYPES_H
 #define DATATYPES_H
 
+#pragma pack(push, 1)
 typedef struct BaroData {
     float altitude = 0.0f;
     float pressure = 0.0f;
     float temperature = 0.0f;
 }BaroData;
+#pragma pack(pop)
 
+#pragma pack(push, 1)
 typedef struct imuData_float{
     float x = 0;
     float y = 0;
     float z = 0;
 }imuData_float;
+#pragma pack(pop)
 
+#pragma pack(push, 1)
 typedef struct imuData_int16{
     int16_t x = 0;
     int16_t y = 0;
     int16_t z = 0;
 }imuData_int16;
+#pragma pack(pop)
 
+#pragma pack(push, 1)
 typedef struct ImuData{
     imuData_float acc;
     imuData_float gyro;
     imuData_float mag;
 }ImuData;
+#pragma pack(pop)
 
-
+#pragma pack(push, 1)
 typedef struct GpsData{
     double latitude = 0.0;
     double longitude = 0.0;
@@ -37,5 +45,6 @@ typedef struct GpsData{
     unsigned long time = 0;
     unsigned long date = 0;
 }GpsData;
+#pragma pack(pop)
 
 #endif
